b3a6dc92ba64279356b2cb709209f6dc7677a081
Secret Roman <romchikgoro@gmail.com>
2024-12-07 20:42:45 +0200

    modifed files

--- 00000001/main.js
+++ 00000001/main.js
@@ -1,16 +1,16 @@
 document.getElementById('main').onclick = function(){
 	
-	window.location.href="";	
+	window.location.href="https://roman112011.github.io/book-search/main.html";	
 }
 document.getElementById('info').onclick = function(){
 	
-	window.location.href="";	
+	window.location.href="https://roman112011.github.io/book-search/html/info.html";	
 }
 document.getElementById('recomend').onclick = function(){
 	
-	window.location.href="";	
+	window.location.href="https://roman112011.github.io/book-search/html/recomend.html";	
 }
 document.getElementById('search').onclick = function(){
 	
-	window.location.href="";	
+	window.location.href="https://roman112011.github.io/book-search/html/search.html";	
 }
