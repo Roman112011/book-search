23f03acd8a13ae968a0a992e2ad0477da6508615
Secret Roman <romchikgoro@gmail.com>
2024-12-07 20:34:54 +0200

    add files

---
+++ 00000001/main.js
@@ -0,0 +1,16 @@
+document.getElementById('main').onclick = function(){
+	
+	window.location.href="";	
+}
+document.getElementById('info').onclick = function(){
+	
+	window.location.href="";	
+}
+document.getElementById('recomend').onclick = function(){
+	
+	window.location.href="";	
+}
+document.getElementById('search').onclick = function(){
+	
+	window.location.href="";	
+}
